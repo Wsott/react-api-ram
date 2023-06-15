60728505515ad87187686693a2d5adbe892f3021
Emiliano Gomez Olivera <theguiltyspark343@outlook.com>
2023-06-15 01:34:30 -0300

    Avance de estilos

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,12 +1,12 @@
-import styles from "../styles/NavigationBar.modules.css";
+import styles from "../styles/NavigationBar.module.css";
 
 function NavigationBar() {
     return(
-        <div>
-            <div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
                 <img src="src\assets\logo.png" alt="Logo de la web" />
             </div>
-            <div>
+            <div className={styles.linkContainer}>
                 <a href="">Personajes</a>
                 <a href="">Ubicaciones</a>
                 <a href="">Episodios</a>
