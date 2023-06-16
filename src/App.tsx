1a19c2ffa412f4c4741e37ebefb0ef01382d8845
Emiliano Gomez Olivera <theguiltyspark343@outlook.com>
2023-06-16 02:13:57 -0300

    Agregada ruta al componente CharacterSection

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/characters' element={<CharacterSection/>} />
+        <Route path='/characters/:index?' element={<CharacterSection/>} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/CharacterSection.tsx
+++ src/components/CharacterSection.tsx
@@ -2,13 +2,16 @@ import { useEffect, useState } from "react";
 import stlyes from "../styles/CharacterSection.module.css";
 import NavigationBar from "./NavigationBar";
 import CharacterCard from "./CharacterCard";
+import { useParams } from "react-router-dom";
 
 
 function CharacterSection () {
     const [data, setData] = useState();
+    const {index} = useParams();
+    const currentIndex: number = Number(index) || 1;
 
     useEffect(() => {
-        loadData(1);
+        loadData(currentIndex);
     }, [])
 
     async function loadData (index: number) {
@@ -26,20 +29,22 @@ function CharacterSection () {
     return (
         <div className={stlyes.container}>
             <NavigationBar></NavigationBar>
-            <div>
-                {
-                    (data)?
-                        data.map((current) => {
-                            return (
-                                <CharacterCard 
-                                    image={current.image} 
-                                    name={current.name} 
-                                    species={current.species}></CharacterCard>
-                            )
-                        })
-                    :
-                    <h1>{"Cargando..."}</h1>
-                }
+            <div className={stlyes.mainSection}>
+                <div className={stlyes.charactersContainer}>
+                    {
+                        (data)?
+                            data.map((current) => {
+                                return (
+                                    <CharacterCard 
+                                        image={current.image} 
+                                        name={current.name} 
+                                        species={current.species}></CharacterCard>
+                                )
+                            })
+                        :
+                        <h1>{"Cargando..."}</h1>
+                    }
+                </div>
             </div>
         </div>
         
