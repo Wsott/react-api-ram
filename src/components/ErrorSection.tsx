e9a0d3fb7a0c8a23533e2671f1e5a78264c7375e
Emiliano Gomez Olivera <theguiltyspark343@outlook.com>
2023-06-21 23:14:17 -0300

    Corregido estilo de la pagina 404

--- src/components/ErrorSection.tsx
+++ src/components/ErrorSection.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "../styles/ErrorSection.module.css";
+import baseStyle from "../styles/CharacterSection.module.css";
 
 interface errorSectionData {
     URL: string
@@ -7,12 +8,14 @@ interface errorSectionData {
 
 function ErrorSection ( {URL}: errorSectionData ) {
     return (
-        <div className={styles.errorContainer}>
-            <h1 className={styles.title}>Error 404</h1>
-            <h2 className={styles.ascii}>¯\_(ツ)_/¯</h2>
-            <p>La pagina que estas buscando no existe; es peligroso seguir adelante 
-                <Link to={URL}><b> USA ESTO</b></Link>
-            </p>
+        <div className={`${baseStyle.mainSectionNoFooter}`}>
+            <div className={styles.errorContainer}>
+                <h1 className={styles.title}>Error 404</h1>
+                <h2 className={styles.ascii}>¯\_(ツ)_/¯</h2>
+                <p>La pagina que estas buscando no existe; es peligroso seguir adelante 
+                    <Link to={URL}><b> USA ESTO</b></Link>
+                </p>
+            </div>
         </div>
     );
 }
